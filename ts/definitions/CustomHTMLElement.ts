3526109428d673c557c0159b1bb92e8c6686552d
Sebastian Streich <streich.mobile@gmail.com>
2015-06-10 15:02:48 +0200

    typescript entfernt

--- ts/definitions/CustomHTMLElement.ts
+++
@@ -1,6 +0,0 @@
-interface CustomHTMLElement extends HTMLElement {
-	createdCallback() : void ;
-	attachedCallback() :void;
-	attributeChangedCallback(attribute,oldVal,newVal) : void;
-	shadowDOM : Document;
-}
--- ts/definitions/PaintOperation.ts
+++
@@ -1,9 +0,0 @@
-interface PaintOperation {
-	time : Number;
-	color : number;
-	stroke: Number;
-	type: number;
-	id: number;
-	xPoints : Int16Array;
-	yPoints : Int16Array;
-}
